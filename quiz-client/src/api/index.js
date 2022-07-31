a4f738846eea1c17e513c42686cecef61a4ac326
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-07-31 23:19:22 +0100

    initial commit

---
+++ quiz-client/src/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const BASE_URL= 'http://localhost:5295/'
+
+export const ENDPOINTS = {
+    participant: 'participant'
+}
+
+export const createAPIEndpoint = endpoint => {
+    let url = BASE_URL + 'api/' + endpoint + '/';
+    return {
+        fetch: () => axios.get(url),
+        fetchById: id => axios.get(url + id),
+        post: newRecord => axios.post(url, newRecord),
+        put: (id, updateRecord) =>axios.post(url + id, updateRecord),
+        delete: id => axios.delete(url + id),
+    }
+}
