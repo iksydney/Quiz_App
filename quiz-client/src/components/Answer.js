3cbd8f06eba4b899f0cfaa417e1eb4e7e1becde6
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-05 23:12:42 +0100

    logout functionality

--- quiz-client/src/components/Answer.js
+++ quiz-client/src/components/Answer.js
@@ -1,22 +1,30 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-  Box,
   CardMedia,
+  Chip,
+  List,
   ListItem,
   Typography,
-  List,
 } from "@mui/material";
+import { Box } from "@mui/system";
 import { BASE_URL } from "../api";
 import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+import { red, green, grey } from "@mui/material/colors";
 
 export default function Answer({ qnAnswers }) {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
+  };
+
+  const markCorrectOrNot = (qna, idx) => {
+    if ([qna.answer, qna.selected].includes(idx)) {
+      return { sx: { color: qna.answer == idx ? green[500] : red[500] } };
+    }
   };
 
   return (
@@ -28,24 +36,32 @@ export default function Answer({ qnAnswers }) {
           expanded={expanded === j}
           onChange={handleChange(j)}
         >
-          <AccordionSummary>
+          <AccordionSummary
+            expandIcon={
+              <ExpandCircleDownIcon
+                sx={{
+                  color: item.answer == item.selected ? green[500] : red[500],
+                }}
+              />
+            }
+          >
             <Typography sx={{ width: "90%", flexShrink: 0 }}>
               {item.qnInWords}
             </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails sx={{ backgroundColor: grey[900] }}>
             {item.imageName ? (
               <CardMedia
                 component="img"
                 image={BASE_URL + "images/" + item.imageName}
-                sx={{ m: "10px auto", width: "70%" }}
+                sx={{ m: "10px auto", width: "auto" }}
               />
             ) : null}
             <List>
               {item.options.map((x, i) => (
                 <ListItem key={i}>
-                  <Typography>
-                    <b>{String.fromCharCode(65 + i) + "."}</b>
+                  <Typography {...markCorrectOrNot(item, i)}>
+                    <b>{String.fromCharCode(65 + i) + ". "}</b>
                     {x}
                   </Typography>
                 </ListItem>
